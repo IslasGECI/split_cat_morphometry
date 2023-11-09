3a2a500d092c062c58033350f202ea603317043d
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-11-09 13:20:33 -0800

    ♻️  Refactor

--- tests/testthat/test_nothing.R
+++ tests/testthat/test_nothing.R
@@ -1,13 +1,12 @@
 describe("Test all is ready", {
+  raw_data <- readr::read_csv("/workdir/tests/data/morfometria_gatos_guadalupe.csv", show_col_types = FALSE)
   it("Only trapping: traps coordinates", {
-    raw_data <- readr::read_csv("/workdir/tests/data/morfometria_gatos_guadalupe.csv", show_col_types = FALSE)
     expected_columns <- c("ID_de_trampa", "Coordenada_Este", "Coordenada_Norte")
     obtained <- select_id_and_coodinates(raw_data)
     obtained_columns <- names(obtained)
     expect_equal(obtained_columns, expected_columns)
   })
   it("Only trapping: date and morphometry", {
-    raw_data <- readr::read_csv("/workdir/tests/data/morfometria_gatos_guadalupe.csv", show_col_types = FALSE)
     not_expected_columns <- c("Coordenada_Este", "Coordenada_Norte", "Zona")
     obtained <- select_date_and_morphometry(raw_data)
     obtained_columns <- names(obtained)
