2fcffe1bb6dbdc534ddbec4df3edbe5c763c7700
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-11-09 14:33:06 -0800

    ✅ Pass tests

--- R/do_nothing.R
+++ R/do_nothing.R
@@ -1,6 +1,7 @@
 select_id_and_coodinates <- function(raw_data) {
   raw_data |>
-    dplyr::select(c("ID_de_trampa", "Coordenada_Este", "Coordenada_Norte"))
+    dplyr::select(c("ID_de_trampa", "Coordenada_Este", "Coordenada_Norte")) |>
+    dplyr::distinct()
 }
 
 select_date_and_morphometry <- function(raw_data) {
