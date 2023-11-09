f4da4ad55d4f3556190e405b8118d2c74660e55f
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2023-11-09 14:16:55 -0800

    ♻️  Refactor

--- R/do_nothing.R
+++ R/do_nothing.R
@@ -1,7 +1,3 @@
-return_one <- function() {
-  return(1)
-}
-
 select_id_and_coodinates <- function(raw_data) {
   raw_data |>
     dplyr::select(c("ID_de_trampa", "Coordenada_Este", "Coordenada_Norte"))
